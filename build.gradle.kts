edeef3f3e7c6b52343b52607b9913b4519c59976
Daineair <sces2859615s@gmail.com>
2023-11-01 15:01:00 +0800

    Update build.gradle.kts
    Add `application` plugin
    Set

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,15 @@
 plugins {
     id("java")
+    application
 }
 
 group = "com.github.Daineair"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass = "com.github.Daineair.Main"
+}
+
 
 repositories {
     mavenCentral()
@@ -13,6 +19,19 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
+
+tasks {
+    javadoc {
+        options.encoding = "UTF-8"
+    }
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+    compileTestJava {
+        options.encoding = "UTF-8"
+    }
+}
+
 
 tasks.test {
     useJUnitPlatform()
